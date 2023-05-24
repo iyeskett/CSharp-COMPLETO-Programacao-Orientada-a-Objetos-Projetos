d87b8afdccb5dae05a7a5f7629f7350f2d198fb3
Felipe Mello <felipesilva.mello@hotmail.com>
2023-05-23 22:52:41 -0300

    Generics PARTE 1

---
+++ Seção 15 - Generics, Set, Dictionary/secao15/generics1/Program.cs
@@ -0,0 +1,15 @@
+﻿using generics1;
+
+PrintService printService = new PrintService();
+
+Console.Write("How many values?");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.addValue(x);
+}
+
+printService.Print();
+Console.WriteLine($"First: {printService.First()}");
