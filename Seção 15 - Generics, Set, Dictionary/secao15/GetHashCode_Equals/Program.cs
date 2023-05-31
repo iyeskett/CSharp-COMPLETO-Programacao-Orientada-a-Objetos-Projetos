1818ef68797942855659247c6711820c0a498c19
Felipe Mello <felipesilva.mello@hotmail.com>
2023-05-31 20:00:25 -0300

    GetHashCode e Equals

---
+++ Seção 15 - Generics, Set, Dictionary/secao15/GetHashCode_Equals/Program.cs
@@ -0,0 +1,8 @@
+﻿using GetHashCode_Equals.Entities;
+
+Client a = new Client { Name = "Marria", Email = "maria@gmail.com"};
+Client b = new Client {Name = "Alex", Email = "alex@gmail.com" };
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
