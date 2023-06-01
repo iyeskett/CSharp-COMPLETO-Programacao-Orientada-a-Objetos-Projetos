c6745040638255d75574105e473e15f1c180883a
Felipe Mello <felipesilva.mello@hotmail.com>
2023-05-31 23:21:28 -0300

    Exercício proposto sobre Dictionary

---
+++ Seção 15 - Generics, Set, Dictionary/secao15/ExercicioDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(',');
+            string name = line[0];
+            int votes = int.Parse(line[1]);
+            if (candidates.ContainsKey(name))
+                candidates[name] += votes;
+            else
+                candidates[name] = votes;
+        }
+
+        foreach (var candidate in candidates)
+        {
+            Console.WriteLine($"{candidate.Key}: {candidate.Value}");
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e); ;
+}
