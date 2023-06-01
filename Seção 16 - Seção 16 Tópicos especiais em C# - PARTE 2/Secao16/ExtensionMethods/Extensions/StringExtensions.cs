575620d699b872e065baffe770cb1ca9f9a8e677
Felipe Mello <felipesilva.mello@hotmail.com>
2023-05-31 23:58:02 -0300

    Extension methods

---
+++ Seção 16 - Seção 16 Tópicos especiais em C# - PARTE 2/Secao16/ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{
+    internal static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+                return thisObj;
+
+            return thisObj.Substring(0, count) + "...";
+        }
+    }
+}
---
+++ Seção 16 - Seção 16 Tópicos especiais em C# - PARTE 2/Secao16/ExtensionMethods/Program.cs
@@ -0,0 +1,8 @@
+﻿// Demo 1
+DateTime dt = new DateTime(2023, 05, 31, 22, 00, 00);
+Console.WriteLine(dt.ElapsedTime());
+
+// Demo 2
+
+string s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
