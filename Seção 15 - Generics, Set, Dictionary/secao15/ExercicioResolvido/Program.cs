367b01d1104553ee41f21e9b110b2f361cf0494a
Felipe Mello <felipesilva.mello@hotmail.com>
2023-05-31 22:14:18 -0300

    Exercício resolvido sobre conjuntos

---
+++ Seção 15 - Generics, Set, Dictionary/secao15/ExercicioResolvido/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExercicioResolvido.Entities;
+
+HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            set.Add(new LogRecord { Username = name, Instant = instant });
+        }
+
+        Console.WriteLine($"Total users: {set.Count}");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e); ;
+}
