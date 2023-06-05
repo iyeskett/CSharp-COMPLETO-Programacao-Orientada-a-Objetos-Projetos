059bac9942dac2320e970f54b8e68fdf61432eac
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-05 20:41:18 -0300

    Predicate (exemplo com RemoveAll)

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Secao17/Predicate/Program.cs
@@ -0,0 +1,20 @@
+﻿using Predicate.Entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("TV", 900.0));
+list.Add(new Product("Mouse", 50.0));
+list.Add(new Product("Tablet", 350.50));
+list.Add(new Product("HD Case", 80.90));
+
+list.RemoveAll(ProductTest);
+
+foreach (Product p in list)
+{
+    Console.WriteLine(p.Name);
+}
+
+static bool ProductTest(Product product)
+{
+    return product.Price >= 100;
+}
