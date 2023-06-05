59d301cac6a98d160bc15be35fea686d3abe5409
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-05 20:33:56 -0300

    Multicast delegates

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Secao17/MulticastDelegate/Program.cs
@@ -0,0 +1,11 @@
+﻿using MulticastDelegate.Services;
+
+BinaryNumericOperation op = CalculationService.ShowSum;
+op += CalculationService.ShowMax;
+
+double a = 10;
+double b = 12;
+
+op(a, b);
+
+internal delegate void BinaryNumericOperation(double n1, double n2);
