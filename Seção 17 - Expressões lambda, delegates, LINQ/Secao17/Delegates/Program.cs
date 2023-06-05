383ce5bbf1c0f271aebc8aed6d099f19be636075
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-05 20:22:13 -0300

    Introdução a delegates

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Secao17/Delegates/Program.cs
@@ -0,0 +1,11 @@
+﻿using Delegates.Services;
+
+BinaryNumericOperation op = CalculationService.Max;
+
+double a = 10;
+double b = 12;
+
+double result = op(a, b);
+Console.WriteLine(result);
+
+internal delegate double BinaryNumericOperation(double n1, double n2);
