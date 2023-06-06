0a116cac2f99b52d974cfa6415eb02436dc7f5f2
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-06 20:26:46 -0300

    Exercício resolvido

---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Secao17/ExercicioResolvido/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace ExercicioResolvido.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void Deconstruct(out string name, out double price)
+        {
+            name = Name; 
+            price = Price;
+        }
+    }
+}
---
+++ Seção 17 - Expressões lambda, delegates, LINQ/Secao17/ExercicioResolvido/Program.cs
@@ -0,0 +1,34 @@
+﻿using ExercicioResolvido.Entities;
+using System.Globalization;
+
+Console.Write("Enter full file path: ");
+string path = Console.ReadLine();
+
+List<Product> products = new List<Product>();
+
+using (StreamReader sr = File.OpenText(path))
+{
+    while (!sr.EndOfStream)
+    {
+        var fields = sr.ReadLine().Split(',');
+        string name = fields[0];
+        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+        products.Add(new Product(name, price));
+    }
+}
+
+var avg = products.Select(p => p.Price).DefaultIfEmpty().Average();
+Console.WriteLine($"Average price: {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+//var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+var names =
+    from p in products
+    where p.Price < avg
+    orderby p.Name descending
+    select p.Name;
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
