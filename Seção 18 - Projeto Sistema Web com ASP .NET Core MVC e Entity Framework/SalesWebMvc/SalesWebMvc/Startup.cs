4eb074b7a709200fe7caa46c54a0194ca4554ebb
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-12 22:00:18 -0300

    CRUD Scaffolding

--- Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Startup.cs
+++ Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Startup.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -27,6 +29,9 @@ namespace SalesWebMvc
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
