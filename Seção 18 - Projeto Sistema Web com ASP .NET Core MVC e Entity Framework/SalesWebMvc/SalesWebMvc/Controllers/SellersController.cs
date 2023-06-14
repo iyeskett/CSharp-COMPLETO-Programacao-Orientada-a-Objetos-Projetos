f3c427903385047d613742c439ec2862f9ab490e
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-13 22:27:39 -0300

    SellerService e FindAll síncrono

--- Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,17 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerServices _sellerServices;
+
+        public SellersController(SellerServices sellerServices)
+        {
+            _sellerServices = sellerServices;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerServices.FindAll();
+            return View(list);
         }
     }
 }
--- Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Startup.cs
+++ Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -34,6 +35,7 @@ namespace SalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerServices>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
