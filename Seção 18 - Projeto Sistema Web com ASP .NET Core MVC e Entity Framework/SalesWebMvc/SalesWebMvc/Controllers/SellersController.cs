60031301ba55655b6a73397ddeccf8c9bbbf6ae9
Felipe Mello <felipesilva.mello27@gmail.com>
2023-06-14 21:11:21 -0300

    Editar vendedor e exceções de serviço personalizadas

--- Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ Seção 18 - Projeto Sistema Web com ASP .NET Core MVC e Entity Framework/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -72,6 +73,40 @@ namespace SalesWebMvc.Controllers
             if (seller == null) return NotFound();
 
             return View(seller);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var seller = _sellerServices.FindById(id.Value);
+            if (seller == null) return NotFound();
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id) return BadRequest();
+
+            try
+            {
+                _sellerServices.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
